680e1b73acc597601d426bfe9e2aa720c2eb5dfc
MarcusVMendes <marcus.mendes89@gmail.com>
2021-12-02 01:49:29 -0300

    Requisito 7 concluido

---
+++ challenges/desafio7.js
@@ -0,0 +1,19 @@
+db.movies.aggregate([
+  { $match: {
+    languages: "English",
+  } },
+  { $unwind: "$cast" },
+  { $group: {
+    _id: "$cast",
+    numeroFilmes: { $sum: 1 },
+    mediaIMDB: { $avg: "$imdb.rating" },
+  } },
+  { $project: {
+    _id: true,
+    numeroFilmes: true,
+    mediaIMDB: { $round: ["$mediaIMDB", 1] },
+  } },
+  { $sort: { numeroFilmes: -1, _id: -1 } },
+]);
+
+// https://docs.mongodb.com/manual/reference/operator/aggregation/unwind/
